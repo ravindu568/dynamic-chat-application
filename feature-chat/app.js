d9c5b260c44dd9f6313759972f692459a11ad01c
Ravindu Wanshanatha <63859006+ravindu568@users.noreply.github.com>
2024-07-06 22:40:32 +0530

    user offline or not

--- feature-chat/app.js
+++ feature-chat/app.js
@@ -8,9 +8,48 @@ const express=require('express');
 
 const app=express();
 
+const User= require('./models/userModel');
+
 const http=require('http').Server(app);
 
 const userRoutes=require('./routes/userRoutes');
+
+//configure socket.io
+
+const io=require('socket.io')(http);
+
+var usp=io.of('/user-namespace');//usp-user name space
+
+usp.on('connection',async function(socket){
+    console.log('User Connected');
+
+    var userId=socket.handshake.auth.token;
+
+    await User.findByIdAndUpdate({_id: userId},{$set:{is_online:'1' }});
+
+    socket.on('disconnect',async function(){
+            console.log('User Disconnected');
+
+            var userId=socket.handshake.auth.token;
+
+            await User.findByIdAndUpdate({_id: userId},{$set:{is_online:'0' }});
+    });
+});
+
+// const { createServer } = require("http");
+// const { Server } = require("socket.io");
+
+// const httpServer = createServer(app);
+// const io = new Server(httpServer,{});
+
+// io.of("connection", (socket) => {
+//     console.log('User Connected');
+
+//     socket.on('disconnect',function(){
+//         console.log('User Disconnected');
+//     })
+// });
+
 
 
 
